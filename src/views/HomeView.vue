dd8763d6f6934759331921c4f34acdeee7066ab8
Jeff Foster <jeff.foster@red-gate.com>
2025-12-25 14:55:28 +0000

    Rename Gym Equipment to Gym Info with updated subtitle and icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,11 +37,11 @@ function startWorkout() {
       <button class="card card-equipment" @click="navigateTo('/equipment')">
         <div class="card-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M20.57 14.86L22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29z"/>
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
           </svg>
         </div>
-        <span class="card-title">Gym Equipment</span>
-        <span class="card-subtitle">Browse equipment</span>
+        <span class="card-title">Gym Info</span>
+        <span class="card-subtitle">Opening hours & equipment</span>
       </button>
 
       <button class="card card-progress" @click="navigateTo('/progress')">
