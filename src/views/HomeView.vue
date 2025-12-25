06ecdb23cb08dafe3ef7e04cba11220f0c6ecfbf
Jeff Foster <jeff.foster@red-gate.com>
2025-12-25 19:06:29 +0000

    style: swap Progress and Gym Info card positions on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -43,16 +43,6 @@ function navigateTo(route: string) {
         <span class="card-subtitle">Manage workout templates</span>
       </button>
 
-      <button class="card card-equipment" @click="navigateTo('/equipment')">
-        <div class="card-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
-          </svg>
-        </div>
-        <span class="card-title">Gym Info</span>
-        <span class="card-subtitle">Opening hours & equipment</span>
-      </button>
-
       <button class="card card-progress" @click="navigateTo('/progress')">
         <div class="card-icon">
           <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="currentColor">
@@ -61,6 +51,16 @@ function navigateTo(route: string) {
         </div>
         <span class="card-title">Progress</span>
         <span class="card-subtitle">Track your gains</span>
+      </button>
+
+      <button class="card card-equipment" @click="navigateTo('/equipment')">
+        <div class="card-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
+          </svg>
+        </div>
+        <span class="card-title">Gym Info</span>
+        <span class="card-subtitle">Opening hours & equipment</span>
       </button>
     </div>
   </div>
